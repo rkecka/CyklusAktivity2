2072e3aea20fe52a71b5ef4ae8d9ee1947af0615
Radoslav Kecka <rkecka@gmail.com>
2018-01-06 11:05:02 +0100

    Initial commit

---
+++ app/src/main/java/eu/kerastudio/cyklusaktivity/StateChangeActivity.java
@@ -0,0 +1,76 @@
+package eu.kerastudio.cyklusaktivity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.util.Log;
+
+public class StateChangeActivity extends AppCompatActivity {
+    private static final String LAD = "ZmenaStavu";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_state_change);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with zour own action",
+                        Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        Log.i(LAD, "OnCreate");
+    }
+
+    @Override
+        protected void onStart() {
+           super.onStart(); Log.i(LAD, "onStart");
+        }
+
+    @Override
+    protected void onResume() {
+        super.onResume(); Log.i(LAD, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause(); Log.i(LAD, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop(); Log.i(LAD, "onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart(); Log.i(LAD, "onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy(); Log.i(LAD, "onDestroy");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        Log.i(LAD, "onSaveInstanceState");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState){
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.i(LAD, "onRestoreInstanceState");
+    }
+}
